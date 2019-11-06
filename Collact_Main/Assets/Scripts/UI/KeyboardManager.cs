104b5c1e42c596f2cc93c634c7c84e0466b16a10
jiho <hoye2210@naver.com>
2019-11-07 02:37:00 +0900

    키보드 매니저 스크립트 생성

---
+++ Collact_Main/Assets/Scripts/UI/KeyboardManager.cs
@@ -0,0 +1,22 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+public class KeyboardManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public InputField pname;  
+     public string player_name;
+
+     public InputField intputfield;
+
+      public void input_name()
+    {
+        player_name = pname.text;
+        Debug.Log(player_name);
+    }
+}
