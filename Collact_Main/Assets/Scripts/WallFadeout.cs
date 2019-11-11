227ad134b6abc85d5039940fceb15d3725e722db
jiho <hoye2210@naver.com>
2019-11-11 22:05:31 +0900

    backwall fadeout

---
+++ Collact_Main/Assets/Scripts/WallFadeout.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallFadeout : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Animator wallanim;
+    private int current_num;
+    void Start()
+    {
+        wallanim = GetComponent<Animator>();
+    }
+
+    private void call_scenes_current()
+    {
+        SceneManager scenes_ = GameObject.Find("Scenes").GetComponent<SceneManager>();
+        current_num = scenes_.current;
+    }
+    private void FixedUpdate()
+    {
+        call_scenes_current();
+        if(current_num == 0)
+        {
+            wallanim.Play("WallFadeout");
+        }
+        else
+        {
+            wallanim.Rebind();
+        }
+    }
+}
